3976f734384763458a218e434a422b4f1b008d69
Neykoor💜 <chinquepapa@gmail.com>
2025-12-20 14:07:19 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import './lib/database.js'
 import { startConnection } from './lib/connection.js'
 import { loadPlugins } from './lib/plugins.js'
 import { handler } from './lib/handler.js'
 
 const start = async () => {
+  global.loadDatabase()
+
   console.log('Loading plugins...')
   await loadPlugins()
 
@@ -10,9 +13,8 @@ const start = async () => {
   const sock = await startConnection()
 
   sock.ev.on('messages.upsert', async (m) => {
-    if (m.type === 'notify') {
-      await handler(sock, m)
-    }
+    if (m.type !== 'notify') return
+    await handler(sock, m)
   })
 
   process.on('uncaughtException', console.error)
