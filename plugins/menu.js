4a99f8394fadb23de0d5a2625bd6c83e75ea0713
Neykoor💜 <chinquepapa@gmail.com>
2025-12-20 17:24:43 -0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,18 +1,22 @@
-import { plugins } from '../lib/plugins.js';
-
 export default {
-    command: ['menu', 'help'],
-    execute: async ({ sock, m }) => {
-        let menuText = 'Available Commands:\n\n';
+  command: ['menu', 'help'],
+  execute: async ({ sock, m }) => {
+    let menuText = 'Available Commands:\n\n'
 
-        // Retrieve unique commands to avoid duplicates in display if multiple aliases exist
-        const uniquePlugins = new Set(plugins.values());
+    const uniquePlugins = new Set(global.plugins.values())
 
-        uniquePlugins.forEach(plugin => {
-            const cmds = Array.isArray(plugin.command) ? plugin.command.join(', ') : plugin.command;
-            menuText += `- /${cmds}\n`;
-        });
+    for (const plugin of uniquePlugins) {
+      const cmds = Array.isArray(plugin.command)
+        ? plugin.command.join(', ')
+        : plugin.command
 
-        await sock.sendMessage(m.key.remoteJid, { text: menuText }, { quoted: m });
+      menuText += `- /${cmds}\n`
     }
-};
+
+    await sock.sendMessage(
+      m.key.remoteJid,
+      { text: menuText },
+      { quoted: m }
+    )
+  }
+}
