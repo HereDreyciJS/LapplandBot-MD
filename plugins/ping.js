d9286db8230ef80ec60809732740a765def104eb
Neykoor💜 <chinquepapa@gmail.com>
2025-12-20 17:26:58 -0800

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,6 +1,20 @@
 export default {
-    command: ['ping'],
-    execute: async ({ sock, m }) => {
-        await sock.sendMessage(m.key.remoteJid, { text: 'Pong!' }, { quoted: m });
-    }
-};
+  command: ['ping'],
+  execute: async ({ sock, m }) => {
+    const start = Date.now()
+
+    const sent = await sock.sendMessage(
+      m.key.remoteJid,
+      { text: '⏱️ Midiendo...' },
+      { quoted: m }
+    )
+
+    const speed = Date.now() - start
+
+    await sock.sendMessage(
+      m.key.remoteJid,
+      { text: `⚡ Velocidad: ${speed} ms` },
+      { quoted: sent }
+    )
+  }
+}
