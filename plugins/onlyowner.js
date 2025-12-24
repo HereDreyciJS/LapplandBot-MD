325275af5f8f0c0221f94cf2da4f42d41a77baa6
Neykoor💜 <chinquepapa@gmail.com>
2025-12-23 23:15:18 -0800

    Update onlyowner.js

--- plugins/onlyowner.js
+++ plugins/onlyowner.js
@@ -1,11 +1,24 @@
-
 export default {
-  command: ['debugowner'],
-  execute: async ({ sock, m, isOwner }) => {
+  command: ['debugjid'],
+  execute: async ({ sock, m }) => {
+    const msg = m
+    const isGroup = msg.key.remoteJid.endsWith('@g.us')
+
+    const rawSender = isGroup
+      ? msg.key.participant
+      : msg.key.remoteJid
+
+    const normalized = rawSender.replace(/\D/g, '')
+
     await sock.sendMessage(
-      m.key.remoteJid,
-      { text: `Owner detectado: ${isOwner}` },
-      { quoted: m }
+      msg.key.remoteJid,
+      {
+        text:
+          `RAW JID:\n${rawSender}\n\n` +
+          `NORMALIZED:\n${normalized}\n\n` +
+          `OWNERS:\n${global.settings.bot.owners.join(', ')}`
+      },
+      { quoted: msg }
     )
   }
 }
