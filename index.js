3f64513d27ddd4faecebbb490fe4170bb7db35e1
Neykoor💜 <chinquepapa@gmail.com>
2025-12-23 22:45:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 import './lib/system/database.js'
 import settings from './settings.js'
+
 import { startConnection } from './lib/connection.js'
 import { loadPlugins } from './lib/plugins.js'
 import { handler } from './handler.js'
 
+import { makeInMemoryStore } from '@whiskeysockets/baileys'
+import pino from 'pino'
+
 const start = async () => {
   global.settings = settings
-
   global.loadDatabase()
 
-  console.log('Loading plugins...')
   await loadPlugins()
 
-  console.log('Starting bot...')
-  const sock = await startConnection()
+  const store = makeInMemoryStore({
+    logger: pino({ level: 'silent' })
+  })
+
+  const sock = await startConnection(store)
+
+  store.bind(sock.ev)
 
   sock.ev.on('messages.upsert', async (m) => {
     if (m.type !== 'notify') return
-    await handler(sock, m)
+    await handler(sock, m, store)
   })
 
   process.on('uncaughtException', console.error)
