878caa588b3a840e6a2c859f6b8e880e5307349c
Neykoor💜 <chinquepapa@gmail.com>
2025-12-24 08:22:52 -0800

    Update lib.js

--- plugins/lib.js
+++ plugins/lib.js
@@ -2,16 +2,18 @@ import { getLidInfo } from '../lib/utils/lid.js'
 
 export default {
   command: ['debugjid'],
+  description: 'Muestra el JID real y su versión normalizada',
   execute: async ({ sock, m }) => {
     const info = getLidInfo(m)
 
+    const text =
+      '✿ *Debug JID*\n' +
+      `> RAW ⴵ ${info.rawJid}\n` +
+      `> NORMALIZED ⴵ ${info.normalized}`
+
     await sock.sendMessage(
       m.key.remoteJid,
-      {
-        text:
-          `RAW JID:\n${info.rawJid}\n\n` +
-          `NORMALIZED:\n${info.normalized}`
-      },
+      { text },
       { quoted: m }
     )
   }
