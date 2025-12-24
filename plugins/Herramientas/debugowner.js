5086849e8954ec1cc925e6fbf29f2a06cb707cbc
Neykoor💜 <chinquepapa@gmail.com>
2025-12-23 23:20:05 -0800

    Create debugowner.js

---
+++ plugins/Herramientas/debugowner.js
@@ -0,0 +1,12 @@
+export default {
+  command: ['debugowner'],
+  execute: async ({ sock, m, isOwner }) => {
+    await sock.sendMessage(
+      m.key.remoteJid,
+      {
+        text: `Owner detectado: ${isOwner}`
+      },
+      { quoted: m }
+    )
+  }
+}
