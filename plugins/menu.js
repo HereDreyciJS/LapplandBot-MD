da0a3401c8887bc998e2ca60868fc57eecf55016
Neykoor💜 <chinquepapa@gmail.com>
2025-12-24 08:32:14 -0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,28 +1,9 @@
-import os from 'os'
-
 export default {
   command: ['menu', 'help'],
   hidden: true,
   execute: async ({ sock, m }) => {
-    const { name, prefix, image } = global.settings.bot
-
-    const uptime = process.uptime() * 1000
-    const time = new Date().toLocaleString('es-MX')
-    const platform = os.platform()
-    const node = process.version
-    const users = Object.keys(global.db?.data?.users || {}).length
-    const owner = global.settings.bot.owners.join(', ')
-
-    const info =
-`*┏━ ${name} ━⊜*
-┃⋄ Fecha :: ${time}
-┃⋄ Developer :: ${owner}
-┃⋄ Tipo :: WhatsApp Bot
-┃⋄ Usuarios :: ${users}
-┃⋄ Sistema :: ${platform} (${node})
-┃⋄ Enlace :: wa.me/${sock.user.id.split(':')[0]}
-┃⋄ Uptime :: ${formatUptime(uptime)}
-┗━━◘`
+    const { bot } = global.settings
+    const { name, prefix, image, newsletter } = bot
 
     const seen = new Set()
 
@@ -41,23 +22,28 @@ export default {
       .join('\n\n')
 
     const text =
-      `${info}\n\n` +
-      `${list}`
+`┏━ ${name} ━⊜
+┃⋄ Tipo :: WhatsApp Bot
+┃⋄ Comandos :: ${seen.size}
+┗━━◘
+
+${list}`
 
     await sock.sendMessage(
       m.key.remoteJid,
       {
         image: image ? { url: image } : undefined,
-        caption: text
+        caption: text,
+        contextInfo: {
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+            newsletterJid: newsletter.jid,
+            serverMessageId: '0',
+            newsletterName: newsletter.name
+          }
+        }
       },
       { quoted: m }
     )
   }
-}
-
-function formatUptime(ms) {
-  const h = Math.floor(ms / 3600000)
-  const m = Math.floor(ms / 60000) % 60
-  const s = Math.floor(ms / 1000) % 60
-  return `${h}h ${m}m ${s}s`
 }
