d564d3bd1fd99369da6751dda06600510fd71f23
Neykoor💜 <chinquepapa@gmail.com>
2025-12-23 23:21:22 -0800

    Update debugowner.js

--- plugins/Herramientas/debugowner.js
+++ plugins/Herramientas/debugowner.js
@@ -1,10 +1,12 @@
 export default {
   command: ['debugowner'],
-  execute: async ({ sock, m, isOwner }) => {
+  execute: async ({ sock, m }) => {
     await sock.sendMessage(
       m.key.remoteJid,
       {
-        text: `Owner detectado: ${isOwner}`
+        text:
+          'OWNERS CONFIG:\n' +
+          JSON.stringify(global.settings.bot.owners, null, 2)
       },
       { quoted: m }
     )
