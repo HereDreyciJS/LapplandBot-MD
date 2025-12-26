0cdcf993f13ad270fcec41f44c61c0b821380259
Drey <huesocalzado9@gmail.com>
2025-12-26 16:32:37 -0500

    Create download-play.js

---
+++ plugins/download-play.js
@@ -0,0 +1,54 @@
+import fetch from 'node-fetch'
+import yts from 'yt-search'
+import ytdl from '@distube/ytdl-core'
+
+export default {
+  command: ['play'],
+  description: 'Descarga música de YouTube',
+  execute: async ({ sock, m, args }) => {
+    const text = args.join(' ')
+    if (!text) return sock.sendMessage(m.key.remoteJid, { text: '¿Qué canción quieres escuchar? 🎶' }, { quoted: m })
+
+    try {
+      
+      const isUrl = text.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/)
+      let videoUrl = text
+      let title = 'Música'
+
+      if (!isUrl) {
+        const search = await yts(text)
+        const video = search.videos[0]
+        if (!video) return sock.sendMessage(m.key.remoteJid, { text: 'No encontré nada 😿' }, { quoted: m })
+        videoUrl = video.url
+        title = video.title
+      }
+
+      
+      await sock.sendMessage(m.key.remoteJid, { text: `⏳ Descargando: *${title}*...` }, { quoted: m })
+
+      
+      const stream = ytdl(videoUrl, { filter: 'audioonly', quality: 'highestaudio' })
+      const chunks = []
+      
+      for await (const chunk of stream) {
+        chunks.push(chunk)
+      }
+      const buffer = Buffer.concat(chunks)
+
+      
+      await sock.sendMessage(
+        m.key.remoteJid,
+        {
+          audio: buffer,
+          mimetype: 'audio/mp4',
+          fileName: `${title}.mp3`
+        },
+        { quoted: m }
+      )
+
+    } catch (e) {
+      console.error(e)
+      sock.sendMessage(m.key.remoteJid, { text: 'Ocurrió un error al descargar el audio ❌' }, { quoted: m })
+    }
+  }
+}
