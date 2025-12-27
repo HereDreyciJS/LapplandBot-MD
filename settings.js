c16668ba8f3569db9edb564adc5ca350073e66d4
Drey <huesocalzado9@gmail.com>
2025-12-27 00:51:42 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,20 @@
-export default {
+import APIs from './lib/apis.js'
+
+const settings = {
   bot: {
     name: 'Lappland Bot',
     prefix: '/',
     public: true,
     autoread: false,
+
     owners: [
       '239754268389439'
     ],
+
+    staffGroups: [
+      '120363422647651943@g.us'
+    ],
+
     image: 'https://files.catbox.moe/t1e8hp.jpeg',
 
     newsletter: {
@@ -15,3 +23,7 @@ export default {
     }
   }
 }
+
+global.APIs = APIs
+
+export default settings
