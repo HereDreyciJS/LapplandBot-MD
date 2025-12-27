56ad0004afbb163883446427c5ff727264d75f1c
Drey <huesocalzado9@gmail.com>
2025-12-26 19:11:49 -0500

    Update download-play.js

--- plugins/download-play.js
+++ plugins/download-play.js
@@ -21,7 +21,7 @@ export default {
         quality: 'highestaudio',
         requestOptions: {
           headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
           }
         }
       })
