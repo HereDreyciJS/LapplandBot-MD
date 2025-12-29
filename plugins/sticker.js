e1515cc5a26089796ae6ad1598abc70b1d707399
Drey <huesocalzado9@gmail.com>
2025-12-28 22:27:37 -0500

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,58 +1,21 @@
-import { downloadContentFromMessage } from '@whiskeysockets/baileys'
-import { Sticker } from 'wa-sticker-formatter'
-
-export default {
-  command: ['s', 'sticker', 'stiker'],
-  description: 'Crea stickers localmente',
-  execute: async ({ sock, m, pushName }) => {
-    try {
-      let quoted =
-        m.message?.extendedTextMessage?.contextInfo?.quotedMessage
-
-      let msg = quoted || m.message
-
-      if (msg?.viewOnceMessageV2) {
-        msg = msg.viewOnceMessageV2.message
-      }
-
-      if (!msg?.imageMessage) {
-        return sock.sendMessage(
-          m.key.remoteJid,
-          { text: 'Responde a una imagen.' },
-          { quoted: m }
-        )
-      }
-
-      const stream = await downloadContentFromMessage(
-        msg.imageMessage,
-        'image'
-      )
-
-      let buffer = Buffer.alloc(0)
-      for await (const chunk of stream) {
-        buffer = Buffer.concat([buffer, chunk])
-      }
-
-      const sticker = new Sticker(buffer, {
-        pack: 'LapplandBot-MD',
-        author: pushName || 'Usuario',
-        type: 'full',
-        quality: 80
+const base64 = buffer.toString('base64')
+      const response = await fetch('https://api.lolhuman.xyz/api/stickerwp?apikey=GataDios', {
+        method: 'POST',
+        body: buffer
       })
 
-      await sock.sendMessage(
-        m.key.remoteJid,
-        { sticker: await sticker.toBuffer() },
-        { quoted: m }
-      )
+      if (!response.ok) throw new Error('Error en la API')
+
+      const stiker = await response.buffer()
+
+     
+      await sock.sendMessage(m.key.remoteJid, { 
+        sticker: stiker 
+      }, { quoted: m })
 
     } catch (e) {
       console.error('Error en Sticker:', e)
-      await sock.sendMessage(
-        m.key.remoteJid,
-        { text: `❌ Error creando sticker` },
-        { quoted: m }
-      )
+      await sock.sendMessage(m.key.remoteJid, { text: '❌ No se pudo crear el sticker. Intenta con una imagen más pequeña.' }, { quoted: m })
     }
   }
 }
