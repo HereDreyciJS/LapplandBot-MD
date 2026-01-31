42f8133d5499cc72d09c3968c36a114c083e8c60
Drey <huesocalzado9@gmail.com>
2026-01-31 11:16:44 -0500

    Update harem.js

--- plugins/harem.js
+++ plugins/harem.js
@@ -4,26 +4,14 @@ export default {
     const chatId = m.key.remoteJid
     const sender = m.key.participant || m.key.remoteJid
 
-    global.db.data.harem ??= {}
+    global.db.data.chats ??= {}
+    const chatDb = global.db.data.chats[chatId]
+    chatDb.harem ??= {}
+    const list = chatDb.harem[sender]
+    
+    if (!list || !list.length) return sock.sendMessage(chatId, { text: '❌ No tienes waifus aún en este grupo' }, { quoted: m })
 
-    const list = global.db.data.harem[sender]
-
-    if (!list || !list.length) {
-      return sock.sendMessage(
-        chatId,
-        { text: '❌ No tienes waifus aún' },
-        { quoted: m }
-      )
-    }
-
-    const text = list.map((c, i) =>
-      `${i + 1}. ${c.name} (${c.series || 'Desconocido'})`
-    ).join('\n')
-
-    await sock.sendMessage(
-      chatId,
-      { text: `💘 *Tu Harem*\n\n${text}` },
-      { quoted: m }
-    )
+    const text = list.map((c, i) => `${i + 1}. ${c.name} (${c.series || 'Desconocido'})`).join('\n')
+    await sock.sendMessage(chatId, { text: `💘 *Tu Harem en este grupo*\n\n${text}` }, { quoted: m })
   }
 }
